8f9735d80b7aeb122b896f466ae731d4963f69dc
D1ceS5 <makdggf@gmail.com>
2020-03-18 18:54:30 +0200

    Message added v1

---
+++ TeamProject_/TeamProject_/Message.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_
+{
+    class Message
+    {
+        static readonly string path = "TeamTestBotDB.sqlite";
+
+        public int ID { get; set; }
+        public int USER_ID { get; set; }
+        public string MSG { get; set; }
+        public bool IS_BOT { get; set; } 
+
+        public static List<Message> GetMessages()
+        {
+            List<Message> result = new List<Message>();
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={path}"))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM MESSAGE", connection))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                result.Add(new Message() { ID = reader.GetInt32(0), USER_ID = reader.GetInt32(1), MSG = reader.GetString(2), IS_BOT = reader.GetBoolean(3) });
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        public static List<Message> GetMessagesByID(int ID)
+        {
+            List<Message> result = new List<Message>();
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={path}"))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM MESSAGE WHERE ID = @id", connection))
+                {
+                    cmd.Parameters.Add(new SQLiteParameter("@id", ID));
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if(reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                result.Add(new Message() { ID = reader.GetInt32(0), USER_ID = reader.GetInt32(1), MSG = reader.GetString(2), IS_BOT = reader.GetBoolean(3) });
+                            }
+                        }
+                       
+                    }
+                }
+            }
+            return result;
+        }
+        public static List<Message> GetMessagesByUserID(int ID)
+        {
+            List<Message> result = new List<Message>();
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={path}"))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM MESSAGE WHERE USER_ID = @id", connection))
+                {
+                    cmd.Parameters.Add(new SQLiteParameter("@id", ID));
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                result.Add(new Message() { ID = reader.GetInt32(0), USER_ID = reader.GetInt32(1), MSG = reader.GetString(2), IS_BOT = reader.GetBoolean(3) });
+                            }
+                        }
+
+                    }
+                }
+            }
+            return result;
+        }
+        public static List<Message> GetMessagesByUserID(Message message)
+        {
+            List<Message> result = new List<Message>();
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={path}"))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM MESSAGE WHERE MSG = @msg", connection))
+                {
+                  
+                    cmd.Parameters.Add(new SQLiteParameter("@msg", message.MSG));
+                    
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                result.Add(new Message() { ID = reader.GetInt32(0), USER_ID = reader.GetInt32(1), MSG = reader.GetString(2), IS_BOT = reader.GetBoolean(3) });
+                            }
+                        }
+
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void AddMessage(int user_id, string message,bool isbot)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={path}"))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand("INSERT INTO MESSAGE(USER_ID,MSG,IS_BOT) VALUES(@uid,@msg,@isb)", connection))
+                {
+                    cmd.Parameters.Add(new SQLiteParameter("@uid", user_id));
+                    cmd.Parameters.Add(new SQLiteParameter("@msg", message));
+                    cmd.Parameters.Add(new SQLiteParameter("@isb", isbot));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
