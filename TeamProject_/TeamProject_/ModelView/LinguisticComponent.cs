ddc1669cfdd43f087bbcabf72aba59a8b9acb0fc
D1ceS5 <makdggf@gmail.com>
2020-03-19 11:24:20 +0200

    struct LingComp added

---
+++ TeamProject_/TeamProject_/ModelView/LinguisticComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject_.Model;
+
+namespace TeamProject_.ModelView
+{
+    struct LinguisticComponent
+    {
+        Question question;
+        List<Answer> answers;
+    }
+}
