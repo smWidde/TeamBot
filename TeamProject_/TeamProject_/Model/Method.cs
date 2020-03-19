07b9cca9dfe49fbe2ccd16b94edd2839a042104b
User <User@WIN-PAI2S3FL083>
2020-03-20 00:39:18 +0200

    Большое обновление!!!

---
+++ TeamProject_/TeamProject_/Model/Method.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject_.Model
+{
+    class Method
+    {
+        string _token;
+        string LINK = "https://api.telegram.org/bot";
+        public Method(string Token)
+        {
+            _token = Token;
+        }
+        public string Getme()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string response = webClient.DownloadString(LINK + _token + "/getMe");
+                return response;
+            }
+        }
+        async public Task SendPhotoIputFile(int ChatID, string pathToPhoto, string catprion = "")
+        {
+            using (MultipartFormDataContent form = new MultipartFormDataContent())
+            {
+                string url = LINK + _token + "/sendPhoto";
+                string fileName = pathToPhoto.Split('\\').Last();
+
+                form.Add(new StringContent(ChatID.ToString(), Encoding.UTF8), "chat_id");
+                form.Add(new StringContent(catprion.ToString(), Encoding.UTF8), "caption");
+                using (FileStream fileStream = new FileStream(pathToPhoto, FileMode.Open, FileAccess.Read))
+                {
+                    form.Add(new StreamContent(fileStream), "photo", fileName);
+                    using (HttpClient client = new HttpClient())
+                        await client.PostAsync(url, form);
+                }
+            }
+
+        }
+        async public Task SendAudioIputFile(int ChatID, string pathToAudio, string catprion = "", int duration = 0, string performer = "", string title = "")
+        {
+            using (MultipartFormDataContent form = new MultipartFormDataContent())
+            {
+                string url = LINK + _token + "/sendAudio";
+                string fileName = pathToAudio.Split('\\').Last();
+
+                form.Add(new StringContent(ChatID.ToString(), Encoding.UTF8), "chat_id");
+                form.Add(new StringContent(catprion.ToString(), Encoding.UTF8), "caption");
+                form.Add(new StringContent(duration.ToString(), Encoding.UTF8), "duration");
+                form.Add(new StringContent(performer.ToString(), Encoding.UTF8), "performer");
+                form.Add(new StringContent(title.ToString(), Encoding.UTF8), "title");
+                using (FileStream fileStream = new FileStream(pathToAudio, FileMode.Open, FileAccess.Read))
+                {
+                    form.Add(new StreamContent(fileStream), "audio", fileName);
+                    using (HttpClient client = new HttpClient())
+                        await client.PostAsync(url, form);
+                }
+            }
+
+        }
+        async public Task SendDocumentIputFile(int ChatID, string pathToDocument, string catprion = "")
+        {
+            using (MultipartFormDataContent form = new MultipartFormDataContent())
+            {
+                string url = LINK + _token + "/sendDocument";
+                string fileName = pathToDocument.Split('\\').Last();
+
+                form.Add(new StringContent(ChatID.ToString(), Encoding.UTF8), "chat_id");
+                form.Add(new StringContent(catprion.ToString(), Encoding.UTF8), "caption");
+                using (FileStream fileStream = new FileStream(pathToDocument, FileMode.Open, FileAccess.Read))
+                {
+                    form.Add(new StreamContent(fileStream), "document", fileName);
+                    using (HttpClient client = new HttpClient())
+                        await client.PostAsync(url, form);
+                }
+            }
+
+        }
+        async public Task SendVideoInputFile(int chatID, string pathToVideo, string caption = "")
+        {
+            using (var form = new MultipartFormDataContent())
+            {
+                string url = string.Format("https://api.telegram.org/bot{0}/sendVideo", _token);
+                string fileName = pathToVideo.Split('\\').Last();
+
+                form.Add(new StringContent(chatID.ToString(), Encoding.UTF8), "chat_id");
+                form.Add(new StringContent(caption, Encoding.UTF8), "caption");
+
+                using (FileStream fileStream = new FileStream(pathToVideo, FileMode.Open, FileAccess.Read))
+                {
+                    form.Add(new StreamContent(fileStream), "video", fileName);
+                    using (var client = new HttpClient())
+                    {
+                        await client.PostAsync(url, form);
+                    }
+                }
+            }
+        }
+    }
+}
--- TeamProject_/TeamProject_/UserWindow.xaml.cs
+++ TeamProject_/TeamProject_/UserWindow.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +17,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TeamProject_.Model;
 using TeamProject_.ModelView;
 using Telegram.Bot;
 
@@ -21,6 +28,8 @@ namespace TeamProject_
     /// </summary>
     public partial class UserWindow : Window
     {
+        Method method = new Method("848578183:AAEyE9rbGZtyq4eunSdruS91Jj-gHn2F9Oc");
+       
         TeleBot bot;
         AnswerCollection answers = new AnswerCollection();
            QuestionCollection qc = new QuestionCollection();    
@@ -28,12 +37,71 @@ namespace TeamProject_
         {
             this.bot = bot;
             InitializeComponent();
+            //string smth = ConfigurationSettings.AppSettings.Get("TeleBotClientKey");
+            //client = new TelegramBotClient("848578183:AAEyE9rbGZtyq4eunSdruS91Jj-gHn2F9Oc");
             labletxt.Content = name;
         }
 
         private void btnsend_Click(object sender, RoutedEventArgs e)
         {
-            bot.SendToOne(Int64.Parse(labletxt.Content.ToString()), msg.Text);
+          bot.SendToOne(Int64.Parse(labletxt.Content.ToString()), msg.Text);
+        }
+
+        private void btnsend_ClickTXT(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                method.SendDocumentIputFile(Convert.ToInt32(labletxt.Content), openfiledialog.FileName);
+            }   
+        }
+
+        private void btnsend_ClickPhoto(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+           
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                method.SendPhotoIputFile(Convert.ToInt32(labletxt.Content), openfiledialog.FileName);
+            }
+        }
+
+        private void btnsend_ClickVideo(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "All Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;" +
+                 "*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;" +
+                 "*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;" +
+                 "*.MIDI;*.RMI;*.MKV";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                method.SendVideoInputFile(Convert.ToInt32(labletxt.Content), openfiledialog.FileName);
+            }
+        }
+
+        private void btnsend_ClickMusic(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "All Supported Audio | *.mp3; *.wma | MP3s | *.mp3 | WMAs | *.wma";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                method.SendAudioIputFile(Convert.ToInt32(labletxt.Content), openfiledialog.FileName);
+            }
+
+        }
+
+        private void btnsend_ClickDoc(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "Pdf Files|*.pdf";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                method.SendDocumentIputFile(Convert.ToInt32(labletxt.Content), openfiledialog.FileName);
+            }
         }
     }
 }
+
+
