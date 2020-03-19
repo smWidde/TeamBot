423292386dcd40bc0ffab78eff631e931315eebe
D1ceS5 <makdggf@gmail.com>
2020-03-19 10:55:32 +0200

    MessageCollection v1

---
+++ TeamProject_/TeamProject_/ModelView/MessageCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject_.Model;
+
+namespace TeamProject_.ModelView
+{
+    class MessageCollection : List<Message>, INotifyCollectionChanged
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public MessageCollection()
+        {
+            foreach (Message item in Message.GetMessages())
+            {
+                Add(item);
+            }
+        }
+        public void AddNewQuestion(int user_id, string message, bool isbot)
+        {
+            Message newOne = Message.AddMessage(user_id,message,isbot);
+            Add(newOne);
+            CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newOne));
+        }
+    }
+}
