9bc1205cdd58e2a66fd097e225bc51b63641c69b
D1ceS5 <makdggf@gmail.com>
2020-03-19 10:33:20 +0200

    Empty telebot class

---
+++ TeamProject_/TeamProject_/ModelView/TeleBot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TeamProject_.ModelView
+{
+    class TeleBot
+    {
+        TelegramBotClient client { get; set; }
+
+        public TeleBot()
+        {
+            client = new TelegramBotClient("1149248725:AAG8ECl7OECLm7TOz6ob2yU1CFVks3LkroA");
+            client.OnMessage += Receive;
+        }
+
+        private void Receive(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+
+            }
+            else if (e.Message.ReplyToMessage != null)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
