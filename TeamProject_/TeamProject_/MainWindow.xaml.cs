19fca5c24f4f5b63f8c8e03e8c7cb15872918438
User <User@WIN-PAI2S3FL083>
2020-03-20 01:31:17 +0200

    Финальное обновление

--- TeamProject_/TeamProject_/MainWindow.xaml.cs
+++ TeamProject_/TeamProject_/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TeamProject_.Model;
 using TeamProject_.ModelView;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -27,12 +29,23 @@ namespace TeamProject_
     {
         static TeleBot client;
         static string path = "TeamTestBotDB.sqlite";
+        Method method = new Method("848578183:AAEyE9rbGZtyq4eunSdruS91Jj-gHn2F9Oc");
+        UserCollection coll = new UserCollection();
         public MainWindow()
         {
             if (!CheckExistDataBase(path))
                 CreateDataBase(path);
+          
             client = new TeleBot("848578183:AAEyE9rbGZtyq4eunSdruS91Jj-gHn2F9Oc");
             InitializeComponent();
+            btnsend_Doc.IsEnabled = false;
+            btnsend_TXT.IsEnabled = false;
+            btnsend_Video.IsEnabled = false;
+            btnsend_PHOTO.IsEnabled = false;
+            btnsend_Music.IsEnabled = false;
+            btnsend_Mass.IsEnabled = false;
+            btnsend_Next.IsEnabled = false;
+
         }
 
        
@@ -109,7 +122,98 @@ namespace TeamProject_
 
         private void SendAll(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("В этой программе можно отпровлять сообщения", "Информация!!!");
+            MessageBox.Show("В главном меню присутствуют 6 кнопок для отправки различных сообщений через телеграм бота", "Информация!!!");
+            MessageBox.Show("Для отправки файла, видео итд выберете нужную кнопку", "Информация!!!");
+            MessageBox.Show("Что бы отправть сообщение нужно ввести символы в текст бокс", "Информация!!!");
+            MessageBox.Show("То что вы выберете отправится всем пользователям использующим телеграм бота", "Информация!!!");
+            MessageBox.Show("Если вы хотите отправить сообщение отдельному пользователю нажмите на кнопку Перейти к пользователям", "Информация!!!");
+            MessageBox.Show("Выберете кому отправлять и дерзайте", "Информация!!!");
+            MessageBox.Show("Удачного пользования!", "Информация!!!");
+            btnsend_Doc.IsEnabled = true;
+            btnsend_TXT.IsEnabled = true;
+            btnsend_Video.IsEnabled = true;
+            btnsend_PHOTO.IsEnabled = true;
+            btnsend_Music.IsEnabled = true;
+            btnsend_Mass.IsEnabled = true;
+            btnsend_Next.IsEnabled = true;
+        }
+
+        private void btnsend_Click(object sender, RoutedEventArgs e)
+        {
             client.SendToEveryone(msg.Text);
+        }
+
+        private void btnsend_ClickTXT(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                foreach (var item in coll)
+                {
+                   method.SendDocumentIputFile(Convert.ToInt32(item.user_id), openfiledialog.FileName);
+                }
+               
+            }
+        }
+
+        private void btnsend_ClickPhoto(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                foreach (var item in coll)
+                {
+                    method.SendPhotoIputFile(Convert.ToInt32(item.user_id), openfiledialog.FileName);
+                }
+            }
+        }
+
+        private void btnsend_ClickVideo(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "All Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;" +
+                 "*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;" +
+                 "*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;" +
+                 "*.MIDI;*.RMI;*.MKV";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                foreach (var item in coll)
+                {
+                    method.SendVideoInputFile(Convert.ToInt32(item.user_id), openfiledialog.FileName);
+                }
+            }
+        }
+
+        private void btnsend_ClickMusic(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "All Supported Audio | *.mp3; *.wma | MP3s | *.mp3 | WMAs | *.wma";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                foreach (var item in coll)
+                {
+                    method.SendAudioIputFile(Convert.ToInt32(item.user_id), openfiledialog.FileName);
+                }
+            }
+
+        }
+
+        private void btnsend_ClickDoc(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openfiledialog = new OpenFileDialog();
+            openfiledialog.Filter = "Pdf Files|*.pdf";
+            if (openfiledialog.ShowDialog().Value == true)
+            {
+                foreach (var item in coll)
+                {
+                    method.SendDocumentIputFile(Convert.ToInt32(item.user_id), openfiledialog.FileName);
+                }
+            }
+
         }
     }
 }
