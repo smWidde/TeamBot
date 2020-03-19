b291fa47f478ed66001b6259c16521cf57016861
Саша Шевченко <casiop8@gmail.com>
2020-03-19 11:17:32 +0200

    class UserCollection added

---
+++ TeamProject_/TeamProject_/ModelView/UserCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject_.Model;
+namespace TeamProject_.ModelView
+{
+    class UserCollection : List<User>, INotifyCollectionChanged
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public UserCollection()
+        {
+            foreach (User item in User.ReadAllUsers())
+            {
+                Add(item);
+            }
+        }
+        public void AddNewQuestion(long User_ID_TG)
+        {
+            User newOne = User.AddUser(User_ID_TG);
+            Add(newOne);
+            CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newOne));
+        }
+    }
+}
